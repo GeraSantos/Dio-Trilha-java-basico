9714bd4986a8260c28ff642da5b5f0ec820daa82
Geraldo Santos <geraldo.gsantos12@gmail.com>
2024-07-17 14:59:44 -0300

    Condicional encadeada

--- estudos/Controle-E-Fluxo/src/ResultadoEscolar.java
+++ estudos/Controle-E-Fluxo/src/ResultadoEscolar.java
@@ -3,10 +3,10 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
-        if(nota >= 7)
+        if(nota >= 7){
             System.out.println("Aprovado");
 
-        else
+        }else
             System.out.println("Reprovado");
 
         // condicional composta
